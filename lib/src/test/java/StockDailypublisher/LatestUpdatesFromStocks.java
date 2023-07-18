95157eaf62285789f1e8c446d5480cd63fef721b
Jakeer <30542099+jakeershaik217@users.noreply.github.com>
2023-07-18 10:11:18 +0530

    Update LatestUpdatesFromStocks.java

--- lib/src/test/java/StockDailypublisher/LatestUpdatesFromStocks.java
+++ lib/src/test/java/StockDailypublisher/LatestUpdatesFromStocks.java
@@ -258,7 +258,11 @@ public class LatestUpdatesFromStocks {
                 props.put("mail.smtp.host", "smtp.office365.com");
                 props.put("mail.smtp.port", "587");
 		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.ssl.trust", "smtp.office365.com");
                 props.put("mail.debug", "true");
+		props.put("mail.smtp.socketFactory.port", "587");
+
+
 		
 		
 
